8dce70049b0c0fcf8086c1ec89eb325deb060c3f
Barrett Powell <bmp@mit.edu>
2022-08-17 10:55:48 -0400

    train_nn.py: added lazy support, added Apix override, returned translation to training loop with check, dec_mask is intersection with lattice_mask and SZD,

--- tomodrgn/commands/train_nn.py
+++ tomodrgn/commands/train_nn.py
@@ -19,7 +19,6 @@ from tomodrgn import dataset
 from tomodrgn import ctf
 from tomodrgn import models
 from tomodrgn import dose
-
 from tomodrgn.lattice import Lattice
 
 log = utils.log
@@ -32,7 +31,7 @@ def add_args(parser):
     parser.add_argument('--load', metavar='WEIGHTS.PKL', help='Initialize training from a checkpoint')
     parser.add_argument('--checkpoint', type=int, default=1, help='Checkpointing interval in N_EPOCHS (default: %(default)s)')
     parser.add_argument('--log-interval', type=int, default=200, help='Logging interval in N_PTCLS (default: %(default)s)')
-    parser.add_argument('-v', '--verbose', action='store_true', help='Increaes verbosity')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Increases verbosity')
     parser.add_argument('--seed', type=int, default=np.random.randint(0, 100000), help='Random seed')
 
     group = parser.add_argument_group('Dataset loading')
@@ -42,12 +41,12 @@ def add_args(parser):
     group.add_argument('--window-r', type=float, default=.85, help='Windowing radius (default: %(default)s)')
     group.add_argument('--datadir', type=os.path.abspath, help='Path prefix to particle stack if loading relative paths from a .star or .cs file')
     group.add_argument('--lazy', action='store_true', help='Lazy loading if full dataset is too large to fit in memory')
+    group.add_argument('--Apix', type=float, default=1.0, help='Override A/px from input starfile; useful if starfile does not have _rlnDetectorPixelSize col')
 
     group.add_argument_group('Tilt series')
     group.add_argument('--do-dose-weighting', action='store_true', help='Flag to calculate losses per tilt per pixel with dose weighting ')
     group.add_argument('--dose-override', type=float, default=None, help='Manually specify dose in e- / A2 / tilt')
     group.add_argument('--do-tilt-weighting', action='store_true', help='Flag to calculate losses per tilt with cosine(tilt_angle) weighting')
-    group.add_argument('--enable-trans', action='store_true', help='Apply translations in starfile. Not recommended if using centered + re-extracted particles')
     group.add_argument('--sample-ntilts', type=int, default=None, help='Number of tilts to sample from each particle per epoch. Default: min(ntilts) from dataset')
 
     group = parser.add_argument_group('Training parameters')
@@ -62,10 +61,11 @@ def add_args(parser):
     group = parser.add_argument_group('Network Architecture')
     group.add_argument('--layers', type=int, default=3, help='Number of hidden layers (default: %(default)s)')
     group.add_argument('--dim', type=int, default=256, help='Number of nodes in hidden layers (default: %(default)s)')
+    group.add_argument('--l-extent', type=float, default=0.5, help='Coordinate lattice size (if not using positional encoding) (default: %(default)s)')
     group.add_argument('--pe-type', choices=('geom_ft','geom_full','geom_lowf','geom_nohighf','linear_lowf', 'gaussian', 'none'), default='geom_lowf', help='Type of positional encoding')
-    group.add_argument('--feat-sigma', type=float, default=0.5, help="Scale for random Gaussian features")
     group.add_argument('--pe-dim', type=int, help='Num sinusoid features in positional encoding (default: D/2)')
     group.add_argument('--activation', choices=('relu', 'leaky_relu'), default='relu', help='Activation (default: %(default)s)')
+    group.add_argument('--feat-sigma', type=float, default=0.5, help="Scale for random Gaussian features")
     group.add_argument('--skip-zeros-decoder', action='store_true', help='Ignore fourier pixels exposed to > 2.5x critical dose')
     group.add_argument('--use-decoder-symmetry', action='store_true', help='Exploit fourier symmetry to only decode half of each image. Reduces GPU memory usage at cost of longer epoch times')
     return parser
@@ -89,25 +89,37 @@ def train(scaler, model, lattice, y, rot, tran, weights, dec_mask, optim, ctf_pa
     optim.zero_grad()
     model.train()
 
-    B = y.size(0)
-    ntilts=y.size(1)
-    D = lattice.D
-    y = y[dec_mask].view(1, -1)
+    B, ntilts, D, D = y.shape
 
     with autocast(enabled=use_amp):
-        # evaluate model at masked fourier components
-        input_coords = (lattice.coords @ rot).view(B,ntilts,D,D,3)[dec_mask,:].unsqueeze(0)  # shape 1 x dec_mask.flat() x 3, because dec_mask can have variable # elements per particle
-        yhat = model(input_coords).view(1,-1) # 1 x B*ntilts*N[final_mask]
+        # translate input images if translations provided
+        if not torch.all(tran == 0):
+            y = lattice.translate_ht(y.view(B * ntilts, -1), tran.view(B * ntilts, 1, 2))
+            y = y.view(B, ntilts, D, D)
+        y = y[dec_mask].view(1, -1)
 
-        if ctf_params is not None:
-            freqs = lattice.freqs2d.unsqueeze(0).expand(B*ntilts,-1,-1) / ctf_params[:,:,1].view(B*ntilts,1,1)
+        # evaluate model at masked fourier components
+        input_coords = lattice.coords @ rot  # shape B x ntilts x D*D x 3
+        input_coords = input_coords[dec_mask.view(B, ntilts, D*D), :]  # flat shape np.sum(dec_mask)*3
+        input_coords = input_coords.unsqueeze(0)  # singleton batch dimension for model to run (dec_mask can have variable # elements per particle, therefore cannot reshape with b > 1)
+
+        # ptcl_pixel_counts = [int(i) for i in torch.sum(dec_mask.view(B, -1), dim=1)]
+        # yhat = [model(input_coords_ptcl) for input_coords_ptcl in input_coords.split(ptcl_pixel_counts, dim=1)]
+        # yhat = torch.cat(yhat, dim=1)
+        yhat = model(input_coords)
+        yhat = yhat.view(1,-1) # 1 x B*ntilts*N[final_mask]
+
+        if not torch.all(ctf_params == 0):
+            freqs = lattice.freqs2d.unsqueeze(0).expand(B * ntilts, -1, -1) / ctf_params[:,:,1].view(B*ntilts,1,1)  # convert freqs to 1/A
             ctf_weights = ctf.compute_ctf(freqs, *torch.split(ctf_params.view(B*ntilts,-1)[:, 2:], 1, 1)).view(B,ntilts,D,D)
             yhat *= ctf_weights[dec_mask].view(1,-1)
 
-        # calculate weighted loss
-        loss = (weights[dec_mask].view(1,-1)*((yhat - y) ** 2)).mean() # reconstruction loss vs input stack weighted by dose+tilt
+        yhat *= weights[dec_mask].view(1,-1)
 
-    # backpropogate loss, update model
+        # calculate weighted loss
+        loss = ((yhat - y) ** 2).mean()  # input loss vs reconstruction weighted by ctf and tilt + dose-dependent amplitude attenuation
+
+    # backpropagate loss, update model
     scaler.scale(loss).backward()
     scaler.step(optim)
     scaler.update()
@@ -187,8 +199,6 @@ def main(args):
         args = get_latest(args, flog)
     flog(' '.join(sys.argv))
     flog(args)
-    # TODO move this to sbatch
-    flog(f'Git revision hash: {utils.check_git_revision_hash("/nobackup/users/bmp/software/tomodrgn/.git")}')
 
     # set the random seed
     np.random.seed(args.seed)
@@ -196,7 +206,7 @@ def main(args):
 
     ## set the device
     use_cuda = torch.cuda.is_available()
-    device = torch.device('cuda:0' if use_cuda else 'cpu')
+    device = torch.device('cuda' if use_cuda else 'cpu')
     flog('Use cuda {}'.format(use_cuda))
     if use_cuda:
         torch.set_default_tensor_type(torch.cuda.FloatTensor)
@@ -211,29 +221,18 @@ def main(args):
         ind = None
 
     # load the particles
-    if args.lazy:
-        raise NotImplementedError
-        # data = dataset.LazyTiltSeriesMRCData(args.particles, norm=args.norm, invert_data=args.invert_data, ind=ind,
-        #                                      window=args.window, datadir=args.datadir, window_r=args.window_r,
-        #                                      do_dose_weighting=args.do_dose_weighting, dose_override=args.dose_override,
-        #                                      do_tilt_weighting = args.do_tilt_weighting)
-
-    else:
-        data = dataset.TiltSeriesDatasetAllParticles(args.particles, norm=args.norm, invert_data=args.invert_data,
-                                                     ind_ptcl=ind, window=args.window, datadir=args.datadir,
-                                                     window_r=args.window_r, do_dose_weighting=args.do_dose_weighting,
-                                                     dose_override=args.dose_override, do_tilt_weighting=args.do_tilt_weighting)
+    data = dataset.TiltSeriesDatasetMaster(args.particles, norm=args.norm, invert_data=args.invert_data,
+                                           ind_ptcl=ind, window=args.window, datadir=args.datadir,
+                                           window_r=args.window_r, do_dose_weighting=args.do_dose_weighting,
+                                           dose_override=args.dose_override, do_tilt_weighting=args.do_tilt_weighting,
+                                           lazy=args.lazy)
     D = data.D
     nptcls = data.nptcls
-    Apix = data.ptcls[data.ptcls_list[0]].ctf[0,1]
+    Apix = data.ptcls[data.ptcls_list[0]].ctf[0,1] if not np.all(data.ptcls[data.ptcls_list[0]].ctf == 0) else args.Apix
+    flog(f'Pixels per particle: {data.ptcls[data.ptcls_list[0]].D ** 2 * data.ptcls[data.ptcls_list[0]].ntilts} ')
 
-    # instantiate model
-    lattice = Lattice(D, extent=0.5)
-    if args.lazy:
-        raise NotImplementedError
-        # flog(f'Pixels per particle (raw data):  {np.prod(data.particles[0][0].get().shape)*Ntilts} ')
-    else:
-        flog(f'Pixels per particle (first particle): {np.prod(data.ptcls[data.ptcls_list[0]].images.shape)} ')
+    # instantiate lattice
+    lattice = Lattice(D, extent=args.l_extent, device=device)
 
     # determine which pixels to decode (cache by related weights_dict keys)
     flog(f'Constructing baseline circular lattice for decoder with radius {lattice.D // 2}')
@@ -242,14 +241,14 @@ def main(args):
         ntilts = data.weights_dict[weights_key].shape[0]
         if args.skip_zeros_decoder:
             # decode pixels with nonzero weights only (can vary by tilt)
-            dec_mask = data.weights_dict[weights_key] != 0  # mask is currently ntilts x D x D
+            dec_mask = (data.weights_dict[weights_key] != 0.0) * lattice_mask.view(1, D, D).cpu().numpy()  # lattice mask excludes DC and > nyquist frequencies; weights mask excludes > optimal dose
             flog(f'Pixels decoded per particle (+ skip-zeros-decoder mask):  {dec_mask.sum()}')
         else:
             # decode pixels within defined circular radius in fourier space (constant for all tilts)
             dec_mask = lattice_mask.view(1, D, D).repeat(ntilts, 1, 1).cpu().numpy()
             flog(f'Pixels decoded per particle (+ fourier circular mask):  {dec_mask.sum()}')
         assert dec_mask.shape == (ntilts, D, D)
-        data.dec_mask_dict[weights_key] = dec_mask
+        data.dec_mask_dict[weights_key] = dec_mask.astype(dtype=bool)
 
     # save plots of all weighting schemes
     for i, weights_key in enumerate(data.weights_dict.keys()):
@@ -269,7 +268,9 @@ def main(args):
 
     # instantiate model
     activation = {"relu": nn.ReLU, "leaky_relu": nn.LeakyReLU}[args.activation]
-    model = models.FTPositionalDecoder(3, lattice.D, args.layers, args.dim, activation, args.pe_type, args.pe_dim, args.amp, args.feat_sigma, args.use_decoder_symmetry)
+    model = models.FTPositionalDecoder(3, lattice.D, args.layers, args.dim, activation, enc_type=args.pe_type, enc_dim=args.pe_dim,
+                                       use_amp=args.amp, feat_sigma=args.feat_sigma, use_decoder_symmetry=args.use_decoder_symmetry)
+    model.to(device)
     flog(model)
     flog('{} parameters in model'.format(sum(p.numel() for p in model.parameters() if p.requires_grad)))
 
@@ -301,7 +302,6 @@ def main(args):
             scaler.load_state_dict(checkpoint['scaler'])
         except:
             flog('No GradScaler instance found in specified checkpoint; creating new GradScaler')
-        model.train()
     else:
         start_epoch = 0
 
@@ -314,17 +314,6 @@ def main(args):
     elif args.multigpu:
         flog(f'WARNING: --multigpu selected, but {torch.cuda.device_count()} GPUs detected')
     model.to(device)
-
-    # apply translations as final preprocessing step, since we aren't optimizing poses
-    if args.enable_trans:
-        flog('Applying translations as final preprocessing step')
-        for ptcl_id in data.ptcls_list:
-            images = torch.tensor(data.ptcls[ptcl_id].images, device='cpu')
-            ntilts = data.ptcls[ptcl_id].ntilts
-            trans = torch.tensor(data.ptcls[ptcl_id].trans, device='cpu')
-            data.ptcls[ptcl_id].images = lattice.translate_ht(images.view(ntilts, -1), trans.unsqueeze(1))
-    else:
-        flog('Not applying translations')
 
     # train
     data_generator = DataLoader(data, batch_size=args.batch_size, shuffle=True)
@@ -340,22 +329,22 @@ def main(args):
             loss_item = train(scaler, model, lattice, batch_images, batch_rot, batch_trans, batch_weights, batch_dec_mask, optim, ctf_params=batch_ctf, use_amp=use_amp)
             loss_accum += loss_item * len(batch_ptcl_ids)
             if batch_it % args.log_interval == 0:
-                flog(f'# [Train Epoch: {epoch+1}/{args.num_epochs}] [{batch_it}/{nptcls} subtomos] Average loss={loss_accum / nptcls:.6f}')
+                flog(f'# [Train Epoch: {epoch+1}/{args.num_epochs}] [{batch_it}/{nptcls} particles]  loss={loss_item:.6f}')
 
-        flog(f'# =====> Epoch: {epoch+1} Average loss = {loss_accum/nptcls:.6}; Finished in {dt.now() - t2}')
+        flog(f'# =====> Epoch: {epoch+1} Average loss = {loss_accum/batch_it:.6}; Finished in {dt.now() - t2}')
         if args.checkpoint and epoch % args.checkpoint == 0:
             flog(f'GPU memory usage: {utils.check_memory_usage()}')
-            out_mrc = '{}/reconstruct.{}.mrc'.format(args.outdir, epoch)
-            out_weights = '{}/weights.{}.pkl'.format(args.outdir, epoch)
+            out_mrc = f'{args.outdir}/reconstruct.{epoch}.mrc'
+            out_weights = f'{args.outdir}/weights.{epoch}.pkl'
             save_checkpoint(model, lattice, optim, epoch, data.norm, Apix, out_mrc, out_weights)
 
     ## save model weights and evaluate the model on 3D lattice
-    out_mrc = '{}/reconstruct.mrc'.format(args.outdir)
-    out_weights = '{}/weights.pkl'.format(args.outdir)
+    out_mrc = f'{args.outdir}/reconstruct.mrc'
+    out_weights = f'{args.outdir}/weights.pkl'
     save_checkpoint(model, lattice, optim, epoch, data.norm, Apix, out_mrc, out_weights)
 
     td = dt.now() - t1
-    flog('Finished in {} ({} per epoch)'.format(td, td / (args.num_epochs - start_epoch)))
+    flog(f'Finished in {td} ({td / (args.num_epochs - start_epoch)} per epoch)')
 
 
 if __name__ == '__main__':
