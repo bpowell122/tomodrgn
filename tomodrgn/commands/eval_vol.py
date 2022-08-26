2d22f9f5cb63a613c8d718d3c5fcc482ba0b8c8c
Barrett Powell <bmp@mit.edu>
2022-08-26 09:06:26 -0400

    clarified documentation and added no_grad+amp to eval_vol.py

--- tomodrgn/commands/eval_vol.py
+++ tomodrgn/commands/eval_vol.py
@@ -8,11 +8,11 @@ from datetime import datetime as dt
 import pprint
 
 import torch
+from torch.cuda.amp import autocast
 
 from tomodrgn import mrc
 from tomodrgn import utils
-from tomodrgn import config
-from tomodrgn.models import HetOnlyVAE, TiltSeriesHetOnlyVAE
+from tomodrgn.models import TiltSeriesHetOnlyVAE
 
 log = utils.log
 vlog = utils.vlog
@@ -21,8 +21,9 @@ def add_args(parser):
     parser.add_argument('weights', help='Model weights')
     parser.add_argument('-c', '--config', metavar='PKL', required=True, help='CryoDRGN config.pkl file')
     parser.add_argument('-o', type=os.path.abspath, required=True, help='Output .mrc or directory')
-    parser.add_argument('--prefix', default='vol_', help='Prefix when writing out multiple .mrc files (default: %(default)s)')
-    parser.add_argument('-v','--verbose',action='store_true',help='Increaes verbosity')
+    parser.add_argument('--prefix', default='vol_', help='Prefix when writing out multiple .mrc files')
+    parser.add_argument('--no-amp', action='store_true', help='Disable use of mixed-precision training')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Increases verbosity')
 
     group = parser.add_argument_group('Specify z values')
     group.add_argument('-z', type=np.float32, nargs='*', help='Specify one z-value')
@@ -32,35 +33,21 @@ def add_args(parser):
     group.add_argument('--zfile', help='Text/.pkl file with z-values to evaluate')
 
     group = parser.add_argument_group('Volume arguments')
-    group.add_argument('--Apix', type=float, default=1, help='Pixel size to add to .mrc header (default: %(default)s A/pix)')
+    group.add_argument('--Apix', type=float, default=1, help='Pixel size to add to output .mrc header')
     group.add_argument('--flip', action='store_true', help='Flip handedness of output volume')
     group.add_argument('--invert', action='store_true', help='Invert contrast of output volume')
     group.add_argument('-d','--downsample', type=int, help='Downsample volumes to this box size (pixels)')
 
-    group = parser.add_argument_group('Overwrite architecture hyperparameters in config.pkl')
-    group.add_argument('--norm', nargs=2, type=float)
-    group.add_argument('-D', type=int, help='Box size')
-    group.add_argument('--enc-layers', dest='qlayers', type=int, help='Number of hidden layers')
-    group.add_argument('--enc-dim', dest='qdim', type=int, help='Number of nodes in hidden layers')
-    group.add_argument('--zdim', type=int,  help='Dimension of latent variable')
-    group.add_argument('--encode-mode', choices=('conv','resid','mlp','tilt'), help='Type of encoder network')
-    group.add_argument('--dec-layers', dest='players', type=int, help='Number of hidden layers')
-    group.add_argument('--dec-dim', dest='pdim', type=int, help='Number of nodes in hidden layers')
-    group.add_argument('--enc-mask', type=int, help='Circular mask radius for image encoder')
-    group.add_argument('--pe-type', choices=('geom_ft','geom_full','geom_lowf','geom_nohighf','linear_lowf','none'), help='Type of positional encoding')
-    group.add_argument('--pe-dim', type=int, help='Num sinusoid features in positional encoding (default: D/2)')
-    group.add_argument('--domain', choices=('hartley','fourier'))
-    group.add_argument('--l-extent', type=float, help='Coordinate lattice size')
-    group.add_argument('--activation', choices=('relu','leaky_relu'), default='relu', help='Activation (default: %(default)s)')
     return parser
 
-def check_inputs(args):
+def check_z_inputs(args):
     if args.z_start:
         assert args.z_end, "Must provide --z-end with argument --z-start"
-    assert sum((bool(args.z), bool(args.z_start), bool(args.zfile))) == 1, "Must specify either -z OR --z-start/--z-end OR --zfile"
+    assert sum((bool(args.z), bool(args.z_start), bool(args.zfile))) == 1, \
+        "Must specify either -z OR --z-start/--z-end OR --zfile"
 
 def main(args):
-    check_inputs(args)
+    check_z_inputs(args)
     t1 = dt.now()
 
     ## set the device
@@ -72,7 +59,7 @@ def main(args):
         log('WARNING: No GPUs detected')
 
     log(args)
-    cfg = config.overwrite_config(args.config, args)
+    cfg = utils.load_pkl(args.config)
     log('Loaded configuration:')
     pprint.pprint(cfg)
 
@@ -84,65 +71,67 @@ def main(args):
         assert args.downsample % 2 == 0, "Boxsize must be even"
         assert args.downsample <= D - 1, "Must be smaller than original box size"
     
-    if 'ntilts' in cfg['dataset_args']:
-        model, lattice = TiltSeriesHetOnlyVAE.load(cfg, args.weights)
-    else:
-        model, lattice = HetOnlyVAE.load(cfg, args.weights)
+    model, lattice = TiltSeriesHetOnlyVAE.load(cfg, args.weights)
     model.eval()
 
-    ### Multiple z ###
-    if args.z_start or args.zfile:
-        
-        ### Get z values
-        if args.z_start:
-            args.z_start = np.array(args.z_start)
-            args.z_end = np.array(args.z_end)
-            z = np.repeat(np.arange(args.n,dtype=np.float32), zdim).reshape((args.n, zdim))
-            z *= ((args.z_end - args.z_start)/(args.n-1))
-            z += args.z_start
-        else:
-            if args.zfile.endswith('.pkl'):
-                z = utils.load_pkl(args.zfile)
-            else:
-                z = np.loadtxt(args.zfile).reshape(-1, zdim)
+    use_amp = not args.no_amp
+    with torch.no_grad():
+        with autocast(enabled=use_amp):
 
-        if not os.path.exists(args.o):
-            os.makedirs(args.o)
+            ### Multiple z ###
+            if args.z_start or args.zfile:
 
-        log(f'Generating {len(z)} volumes')
-        for i,zz in enumerate(z):
-            log(zz)
-            if args.downsample:
-                extent = lattice.extent * (args.downsample/(D-1))
-                vol = model.decoder.eval_volume(lattice.get_downsample_coords(args.downsample+1), 
-                                                args.downsample+1, extent, norm, zz) 
+                ### Get z values
+                if args.z_start:
+                    args.z_start = np.array(args.z_start)
+                    args.z_end = np.array(args.z_end)
+                    z = np.repeat(np.arange(args.n,dtype=np.float32), zdim).reshape((args.n, zdim))
+                    z *= ((args.z_end - args.z_start)/(args.n-1))
+                    z += args.z_start
+                else:
+                    if args.zfile.endswith('.pkl'):
+                        z = utils.load_pkl(args.zfile)
+                    else:
+                        z = np.loadtxt(args.zfile).reshape(-1, zdim)
+                    assert z.shape[1] == zdim
+
+                if not os.path.exists(args.o):
+                    os.makedirs(args.o)
+
+                log(f'Generating {len(z)} volumes')
+                for i,zz in enumerate(z):
+                    log(zz)
+                    if args.downsample:
+                        extent = lattice.extent * (args.downsample/(D-1))
+                        vol = model.decoder.eval_volume(lattice.get_downsample_coords(args.downsample+1),
+                                                        args.downsample+1, extent, norm, zz)
+                    else:
+                        vol = model.decoder.eval_volume(lattice.coords, lattice.D, lattice.extent, norm, zz)
+                    out_mrc = f'{args.o}/{args.prefix}{i:03d}.mrc'
+                    if args.flip:
+                        vol = vol[::-1]
+                    if args.invert:
+                        vol *= -1
+                    mrc.write(out_mrc, vol.astype(np.float32), Apix=args.Apix)
+
+            ### Single z ###
             else:
-                vol = model.decoder.eval_volume(lattice.coords, lattice.D, lattice.extent, norm, zz) 
-            out_mrc = '{}/{}{:03d}.mrc'.format(args.o, args.prefix, i)
-            if args.flip:
-                vol = vol[::-1]
-            if args.invert:
-                vol *= -1
-            mrc.write(out_mrc, vol.astype(np.float32), Apix=args.Apix)
-    
-    ### Single z ###
-    else:
-        z = np.array(args.z)
-        log(z)
-        if args.downsample:
-            extent = lattice.extent * (args.downsample/(D-1))
-            vol = model.decoder.eval_volume(lattice.get_downsample_coords(args.downsample+1), 
-                                            args.downsample+1, extent, norm, z) 
-        else:
-            vol = model.decoder.eval_volume(lattice.coords, lattice.D, lattice.extent, norm, z) 
-        if args.flip:
-            vol = vol[::-1]
-        if args.invert:
-            vol *= -1
-        mrc.write(args.o, vol.astype(np.float32), Apix=args.Apix)
+                z = np.array(args.z)
+                log(z)
+                if args.downsample:
+                    extent = lattice.extent * (args.downsample/(D-1))
+                    vol = model.decoder.eval_volume(lattice.get_downsample_coords(args.downsample+1),
+                                                    args.downsample+1, extent, norm, z)
+                else:
+                    vol = model.decoder.eval_volume(lattice.coords, lattice.D, lattice.extent, norm, z)
+                if args.flip:
+                    vol = vol[::-1]
+                if args.invert:
+                    vol *= -1
+                mrc.write(args.o, vol.astype(np.float32), Apix=args.Apix)
 
     td = dt.now()-t1
-    log('Finished in {}'.format(td))
+    log(f'Finished in {td}')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__)
