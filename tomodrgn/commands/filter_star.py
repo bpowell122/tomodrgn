4ea4d91697c0947b4fc74ed54531cf629059dd20
Barrett Powell <bmp@mit.edu>
2022-09-26 13:57:48 -0400

    added starfile-source option to filter_star.py for relion31 compatibility

--- tomodrgn/commands/filter_star.py
+++ tomodrgn/commands/filter_star.py
@@ -13,6 +13,7 @@ def add_args(parser):
     parser.add_argument('--tomo-id-col', type=str, default='_rlnMicrographName', help='Name of column in input starfile with unique values per tomogram')
     parser.add_argument('--ptcl-id-col', type=str, default='_rlnGroupName', help='Name of column in input starfile with unique values per particle, '
                                                                                  'if `index` then each row is treated as a unique particle')
+    parser.add_argument('--starfile-source', type=str, default='warp', choices=('warp', 'relion31'), help='Software that created starfile; used to identify particles data block')
     parser.add_argument('--ind', help='selected indices array (.pkl)')
     parser.add_argument('--ind-type', choices=('particle', 'image'), default='particle',
                         help='use indices to filter by particle or by individual image')
@@ -38,7 +39,13 @@ def main(args):
     # ingest star file
     in_star = starfile.GenericStarfile(args.input)
     print(f'Loaded starfile : {args.input}')
-    print(f'{len(in_star.blocks["data_"])} rows in input star file')
+    if args.starfile_source == 'warp':
+        ptcl_block = 'data_'
+    elif args.starfile_source == 'relion31':
+        ptcl_block = 'data_particles'
+    else:
+        raise ValueError
+    print(f'{len(in_star.blocks[ptcl_block])} rows in input star file')
     print(f'Filtering action is : {args.action}')
 
     # load filtering selection
@@ -53,21 +60,21 @@ def main(args):
 
     # configure filtering for input data type
     unique_tomo_header = args.tomo_id_col
-    assert unique_tomo_header in in_star.blocks['data_'].columns, f'{unique_tomo_header} not found in starfile `data_` block'
+    assert unique_tomo_header in in_star.blocks[ptcl_block].columns, f'{unique_tomo_header} not found in starfile {ptcl_block} block'
     if args.ptcl_id_col == 'index':
         unique_particle_header = None
     else:
         unique_particle_header = args.ptcl_id_col
-        assert unique_particle_header in in_star.blocks['data_'].columns, f'{unique_particle_header} not found in starfile `data_` block'
+        assert unique_particle_header in in_star.blocks[ptcl_block].columns, f'{unique_particle_header} not found in starfile {ptcl_block} block'
 
     if args.ind:
         # what stride / df column to which to apply the indices
         if args.ind_type == 'particle':
             # indexed per particle
             if unique_particle_header is not None:
-                unique_particles = in_star.blocks['data_'][unique_particle_header].unique()
+                unique_particles = in_star.blocks[ptcl_block][unique_particle_header].unique()
             else:
-                unique_particles = in_star.blocks['data_'].index
+                unique_particles = in_star.blocks[ptcl_block].index
             all_ind = np.arange(len(unique_particles))
             ind_to_drop = check_invert_indices(args, in_ind, all_ind)
 
@@ -78,29 +85,29 @@ def main(args):
             # execute
             particles_to_drop = unique_particles[ind_to_drop]
             if unique_particle_header is not None:
-                in_star.blocks['data_'] = in_star.blocks['data_'][~in_star.blocks['data_'][unique_particle_header].isin(particles_to_drop)]
+                in_star.blocks[ptcl_block] = in_star.blocks[ptcl_block][~in_star.blocks[ptcl_block][unique_particle_header].isin(particles_to_drop)]
             else:
-                in_star.blocks['data_'].drop(particles_to_drop, inplace=True)
-            print(f'{len(in_star.blocks["data_"])} rows after filtering by particle indices')
+                in_star.blocks[ptcl_block].drop(particles_to_drop, inplace=True)
+            print(f'{len(in_star.blocks[ptcl_block])} rows after filtering by particle indices')
 
         elif args.ind_type == 'image':
             # indexed per individual image
             assert args.input_type == 'warp_imageseries', 'A volumeseries starfile cannot filter on a per-image basis because each row represents a volume'
-            all_ind = in_star.blocks['data_'].index.to_numpy()
+            all_ind = in_star.blocks[ptcl_block].index.to_numpy()
             ind_to_drop = check_invert_indices(args, in_ind, all_ind)
 
             # validate inputs
-            assert in_ind.max() < all_ind.max(), 'A supplied index exceeds maximum index of data_ block'
+            assert in_ind.max() < all_ind.max(), f'A supplied index exceeds maximum index of {ptcl_block} block'
             assert in_ind.min() >= 0, 'A supplied index is negative which is not valid'
 
             # execute
-            in_star.blocks['data_'].drop(ind_to_drop, in_place=True)
-            print(f'{len(in_star.blocks["data_"])} rows after filtering by image indices')
+            in_star.blocks[ptcl_block].drop(ind_to_drop, in_place=True)
+            print(f'{len(in_star.blocks[ptcl_block])} rows after filtering by image indices')
 
         elif args.ind_type == 'tilt':
             # indexed per tilt, range of [0, ntilts], assumes all particles have same number of tilts in same order
             assert args.input_type == 'warp_imageseries', 'A volumeseries starfile cannot filter on a per-image basis because each row represents a volume'
-            ntilts = in_star.blocks['data_']['_rlnGroupName'].value_counts().unique()
+            ntilts = in_star.blocks[ptcl_block]['_rlnGroupName'].value_counts().unique()
             assert len(ntilts) == 1, 'All particles must have the same number of tilt images to filter by tilt index'
             all_ind = np.arange(ntilts)
             ind_to_drop = check_invert_indices(args, in_ind, all_ind)
@@ -110,19 +117,19 @@ def main(args):
             assert in_ind.min() >= 0, 'A supplied index is negative which is not valid'
 
             # execute
-            nimages = in_star.blocks['data_'].max()
+            nimages = in_star.blocks[ptcl_block].max()
             tilts_to_drop = [i for i in range(nimages) if i%ntilts in ind_to_drop]
-            in_star.blocks['data_'].drop(tilts_to_drop, in_place=True)
-            print(f'{len(in_star.blocks["data_"])} rows after filtering by tilt indices')
+            in_star.blocks[ptcl_block].drop(tilts_to_drop, in_place=True)
+            print(f'{len(in_star.blocks[ptcl_block])} rows after filtering by tilt indices')
 
     if args.tomogram:
         if args.tomogram == 'all':
             # write each tomo's starfile out separately
-            tomos_to_write = in_star.blocks['data_'][unique_tomo_header].unique()
+            tomos_to_write = in_star.blocks[ptcl_block][unique_tomo_header].unique()
             for tomo in tomos_to_write:
                 individual_outstar = copy.deepcopy(in_star)
-                individual_outstar.blocks['data_'] = individual_outstar.blocks['data_'][individual_outstar.blocks['data_'][unique_tomo_header].str.contains(tomo)]
-                print(f'{len(individual_outstar.blocks["data_"])} rows after filtering by tomogram {tomo}')
+                individual_outstar.blocks[ptcl_block] = individual_outstar.blocks[ptcl_block][individual_outstar.blocks[ptcl_block][unique_tomo_header].str.contains(tomo)]
+                print(f'{len(individual_outstar.blocks[ptcl_block])} rows after filtering by tomogram {tomo}')
                 if args.o.endswith('.star'):
                     outpath = args.o.split('.star')[0]
                 else:
@@ -131,10 +138,10 @@ def main(args):
         else:
             # write out star file for specified tomogram only
             if args.action == 'keep':
-                in_star.blocks['data_'] = in_star.blocks['data_'][in_star.blocks['data_'][unique_tomo_header].str.contains(args.tomogram)]
+                in_star.blocks[ptcl_block] = in_star.blocks[ptcl_block][in_star.blocks[ptcl_block][unique_tomo_header].str.contains(args.tomogram)]
             elif args.action == 'drop':
-                in_star.blocks['data_'] = in_star.blocks['data_'][~in_star.blocks['data_'][unique_tomo_header].str.contains(args.tomogram)]
-            print(f'{len(in_star.blocks["data_"])} rows after filtering by tomogram')
+                in_star.blocks[ptcl_block] = in_star.blocks[ptcl_block][~in_star.blocks[ptcl_block][unique_tomo_header].str.contains(args.tomogram)]
+            print(f'{len(in_star.blocks[ptcl_block])} rows after filtering by tomogram')
 
     in_star.write(args.o)
 
