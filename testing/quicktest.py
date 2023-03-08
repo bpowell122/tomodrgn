781ec1d8073f9c8a1fc864e88bdbf9a47c48d1e0
Barrett Powell <bmp@mit.edu>
2023-03-08 15:45:59 -0500

    quicktest runs with smaller hardware requirements

--- testing/quicktest.py
+++ testing/quicktest.py
@@ -18,8 +18,8 @@ def main():
     tester = CommandTester(workdir)
 
     # add the tests
-    tester.commands.append('tomodrgn train_vae data/10076_both_32_sim.star -o output/01_vae_both_sim --zdim 8 --uninvert-data --seed 42 --log-interval 100 --enc-dim-A 64 --enc-layers-A 2 --out-dim-A 64 --enc-dim-B 32 --enc-layers-B 4 --dec-dim 256 --dec-layers 3 -n 10')
-    tester.commands.append('tomodrgn analyze output/01_vae_both_sim 9 --Apix 13.1 --ksample 2')
+    tester.commands.append('tomodrgn train_vae data/10076_both_32_sim.star -o output/01_vae_both_sim --zdim 8 --uninvert-data --seed 42 --log-interval 100 --enc-dim-A 64 --enc-layers-A 2 --out-dim-A 64 --enc-dim-B 32 --enc-layers-B 4 --dec-dim 16 --dec-layers 3 -n 5')
+    tester.commands.append('tomodrgn analyze output/01_vae_both_sim 4 --Apix 13.1 --ksample 2')
 
     # run the tests
     tester.run()
