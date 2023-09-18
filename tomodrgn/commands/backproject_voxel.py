b0bec8c4d053ed481104374bf4282796136bedda
bpowell122 <81090946+bpowell122@users.noreply.github.com>
2023-09-18 16:46:22 -0400

    refactored backproject_voxel.py to use TiltSeriesMRCData for consistent metadata handling, and half-maps for FSC-filtering

--- tomodrgn/commands/backproject_voxel.py
+++ tomodrgn/commands/backproject_voxel.py
@@ -1,12 +1,13 @@
 '''
-Backproject 2-D images to form unfiltered 3-D reconstruction
+Backproject 2-D images to form 3-D reconstruction with optional filtering and weighting
 '''
 
 import argparse
 import os
-import time
 import numpy as np
+import matplotlib.pyplot as plt
 import torch
+from torch.utils.data import DataLoader
 
 from tomodrgn import utils, mrc, fft, dataset, ctf, starfile
 from tomodrgn.lattice import Lattice
@@ -21,7 +22,13 @@ def add_args(parser):
     group.add_argument('--uninvert-data', dest='invert_data', action='store_false', help='Do not invert data sign')
     group.add_argument('--datadir', type=os.path.abspath, help='Path prefix to particle stack if loading relative paths star file')
     group.add_argument('--ind', type=os.path.abspath, help='Indices of which particles to backproject corresponding tilt images (pkl)')
-    group.add_argument('--first', type=int, default=10000, help='Backproject the first N images')
+    group.add_argument('--first', type=int, help='Backproject the first N particles (default is all)')
+
+    group = parser.add_argument_group('Reconstruction options')
+    group.add_argument('--recon-tilt-weight', action='store_true', help='Weight images in fourier space by cosine(tilt_angle)')
+    group.add_argument('--recon-dose-weight', action='store_true', help='Weight images in fourieri space per tilt per pixel by dose dependent amplitude attenuation')
+    group.add_argument('--lowpass', type=float, default=None, help='Lowpass filter reconstructed volume to this resolution in Angstrom. Defaults to FSC=0.143 correlation between half-maps')
+    group.add_argument('--flip', action='store_true', help='Flip handedness of output volume')
 
     return parser
 
@@ -49,7 +56,6 @@ def add_slice(V, counts, ff_coord, ff, D):
 def main(args):
     assert args.o.endswith('.mrc')
 
-    t1 = time.time()    
     log(args)
     if not os.path.exists(os.path.dirname(args.o)):
         os.makedirs(os.path.dirname(args.o))
@@ -59,79 +65,175 @@ def main(args):
 
     # load the star file
     ptcls_star = starfile.TiltSeriesStarfile.load(args.particles)
-    ptcls_to_imgs_ind = ptcls_star.get_ptcl_img_indices()
-    ptcls_to_imgs_ind = np.array(ptcls_to_imgs_ind, dtype=object)  # convert list -> numpy array to allow multiindexing
 
+    # filter by indices and/or manual selection of first N particles
     if args.ind is not None:
-        ind_ptcls = np.array(utils.load_pkl(args.ind))
-        ind_imgs = ptcls_to_imgs_ind[ind_ptcls].flatten().astype(int)
-        ptcls_star.df = ptcls_star.df.iloc[ind_imgs].reset_index()
-
-    ind_imgs = ptcls_to_imgs_ind.flatten().astype(int)
-
-    # lazily load particle images and filter by ind.pkl, if applicable
-    data = dataset.LazyMRCData(args.particles, norm=(0,1), invert_data=args.invert_data, datadir=args.datadir)
-    data.particles = [data.particles[i] for i in ind_imgs]
-    D = data.D
-    Nimg = ind_imgs.shape[0]
-
-    # Load poses (from pre-filtered dataframe)
-    rots_columns = ['_rlnAngleRot', '_rlnAngleTilt', '_rlnAnglePsi']
-    euler = ptcls_star.df[rots_columns].to_numpy(dtype=np.float32)
-    rots = np.asarray([utils.R_from_relion(*x) for x in euler], dtype=np.float32)
-    rots = torch.from_numpy(rots).to(device)
-
-    trans_columns = ['_rlnOriginX', '_rlnOriginY']
-    if np.all([trans_column in ptcls_star.headers for trans_column in trans_columns]):
-        trans = ptcls_star.df[trans_columns].to_numpy(dtype=np.float32)
-        trans = torch.from_numpy(trans).to(device)
+        log(f'Reading supplied particle indices {args.ind}')
+        ind_ptcls = utils.load_pkl(args.ind)
+        if args.first:
+            ind_ptcls = ind_ptcls[:args.first]
     else:
-        trans = None
+        if args.first:
+            ind_ptcls = np.arange(args.first)
+        else:
+            ind_ptcls = np.arange(len(ptcls_star.get_ptcl_img_indices()))
 
-    # Load CTF (from pre-filtered dataframe)
-    ctf_columns = ['_rlnDetectorPixelSize', '_rlnDefocusU', '_rlnDefocusV', '_rlnDefocusAngle', '_rlnVoltage', '_rlnSphericalAberration',
-                   '_rlnAmplitudeContrast', '_rlnPhaseShift']
-    if np.all([ctf_column in ptcls_star.headers for ctf_column in ctf_columns]):
-        ctf_params = ptcls_star.df[ctf_columns].to_numpy(dtype=np.float32)
-        ctf_params = torch.tensor(ctf_params).to(device)
-    else:
-        ctf_params = None
-    Apix = ctf_params[0,0] if ctf_params is not None else 1
+    # split ind_ptcls by into half sets for calculation of map-map FSC
+    log('Creating random split of selected indices for calculation of map-map FSC')
+    ind_ptcls_half1 = np.sort(np.random.choice(ind_ptcls, size=len(ind_ptcls)//2, replace=False))
+    ind_ptcls_half2 = np.sort(np.array(list(set(ind_ptcls) - set(ind_ptcls_half1))))
+
+    data_half1 = dataset.TiltSeriesMRCData(args.particles,
+                                           norm=(0,1),
+                                           invert_data=args.invert_data,
+                                           ind_ptcl=ind_ptcls_half1,
+                                           window=False,
+                                           datadir=args.datadir,
+                                           recon_dose_weight=args.recon_dose_weight,
+                                           recon_tilt_weight=args.recon_tilt_weight,
+                                           l_dose_mask=False,
+                                           lazy=False,
+                                           sequential_tilt_sampling=True)
+    data_half2 = dataset.TiltSeriesMRCData(args.particles,
+                                           norm=(0,1),
+                                           invert_data=args.invert_data,
+                                           ind_ptcl=ind_ptcls_half2,
+                                           window=False,
+                                           datadir=args.datadir,
+                                           recon_dose_weight=args.recon_dose_weight,
+                                           recon_tilt_weight=args.recon_tilt_weight,
+                                           l_dose_mask=False,
+                                           lazy=False,
+                                           sequential_tilt_sampling=True)
+    D = data_half1.D
+    Apix = ptcls_star.get_tiltseries_pixelsize()
 
     # instantiate lattice
     lattice = Lattice(D, extent=D//2, device=device)
     mask = lattice.get_circular_mask(D//2)
 
-    # instantiate volume
-    V = torch.zeros((D,D,D), device=device)
-    counts = torch.zeros((D,D,D), device=device)
+    # instantiate volumes
+    V_half1 = torch.zeros((D,D,D), device=device)
+    counts_half1 = torch.zeros((D,D,D), device=device)
+    V_half2 = torch.zeros((D,D,D), device=device)
+    counts_half2 = torch.zeros((D,D,D), device=device)
 
-    if args.first:
-        args.first = min(args.first, Nimg)
-        iterator = range(args.first)
-    else:
-        iterator = range(Nimg)
+    # run the backprojection
+    def backproject_dataset(data, V, counts):
 
-    for ii in iterator:
-        if ii%100==0: log(f'image {ii}')
-        r = rots[ii]
-        t = trans[ii] if trans is not None else None
-        ff = torch.tensor(data.get(ii), device=device).view(-1)[mask]
-        if ctf_params is not None:
-            freqs = lattice.freqs2d/ctf_params[ii,0]
-            c = ctf.compute_ctf(freqs, *ctf_params[ii,1:]).view(-1)[mask]
-            ff *= c.sign()
-        if t is not None:
-            ff = lattice.translate_ht(ff.view(1,-1),t.view(1,1,2), mask).view(-1)
-        ff_coord = lattice.coords[mask] @ r
-        add_slice(V, counts, ff_coord, ff, D)
+        B = 1
+        n_ptcls_backprojected = 0
+        data_generator = DataLoader(data, batch_size=B, shuffle=False)
 
-    td = time.time()-t1
-    log(f'Backprojected {len(iterator)} images in {td}s ({td/Nimg}s per image)')
+        for batch_images, batch_rot, batch_trans, batch_ctf, batch_frequency_weights, _, batch_indices in data_generator:
+            # logging
+            n_ptcls_backprojected += len(batch_indices)
+            if n_ptcls_backprojected % 100 == 0: log(f'    {n_ptcls_backprojected} / {data.nptcls} particles')
+
+            # transfer to GPU
+            batch_images = batch_images.to(device)
+            batch_rot = batch_rot.to(device)
+            batch_trans = batch_trans.to(device)
+            batch_ctf = batch_ctf.to(device)
+            batch_frequency_weights = batch_frequency_weights.to(device)
+            ntilts = batch_images.shape[1]
+
+            # correct for translations
+            if not torch.all(batch_trans == 0):
+                batch_images = lattice.translate_ht(batch_images.view(B * ntilts, -1), batch_trans.view(B * ntilts, 1, 2))
+
+            # correct CTF by phase flipping images
+            if not torch.all(batch_ctf == 0):
+                freqs = lattice.freqs2d.unsqueeze(0).expand(B * ntilts, *lattice.freqs2d.shape)
+                freqs = freqs / batch_ctf[:, :, 1].view(B * ntilts, 1, 1)  # convert units from 1/px to 1/Angstrom
+                ctf_weights = ctf.compute_ctf(freqs, *torch.split(batch_ctf.view(B * ntilts, -1)[:, 2:], 1, 1))
+                batch_images = batch_images.view(B, ntilts, D * D) * ctf_weights.view(B, ntilts, D * D).sign()
+
+            # weight by dose and tilt
+            batch_images = batch_images * batch_frequency_weights.view(B, ntilts, D * D)
+
+            # mask out frequencies greater than nyquist
+            batch_images = batch_images.view(B, ntilts, D * D)[:,:,mask]
+
+            # backproject
+            batch_images_coords = lattice.coords[mask] @ batch_rot
+            for i in range(B):
+                for j in range(ntilts):
+                    add_slice(V, counts, batch_images_coords[i,j], batch_images[i,j], D)
+
+        return V, counts
+    log('Backprojecting half set 1 ...')
+    V_half1, counts_half1 = backproject_dataset(data_half1, V_half1, counts_half1)
+    log('Backprojecting half set 2 ...')
+    V_half2, counts_half2 = backproject_dataset(data_half2, V_half2, counts_half2)
+
+    # reconstruct full and half-maps
+    log('Reconstructing...')
+    V = V_half1 + V_half2
+    counts = counts_half1 + counts_half2
+
     counts[counts == 0] = 1
     V /= counts
-    V = fft.ihtn_center(V[0:-1,0:-1,0:-1].cpu().numpy())
-    mrc.write(args.o,V.astype('float32'), Apix=Apix)
+    counts_half1[counts_half1 == 0] = 1
+    V_half1 /= counts_half1
+    counts_half2[counts_half2 == 0] = 1
+    V_half2 /= counts_half2
+
+    # calculate map-map FSC
+    threshold_correlation = 0.5
+    x, fsc = utils.calc_fsc(fft.ihtn_center(V_half1[0:-1,0:-1,0:-1].cpu().numpy()),
+                            fft.ihtn_center(V_half2[0:-1,0:-1,0:-1].cpu().numpy()),
+                            mask='soft')
+    threshold_resolution = x[-1] if np.all(fsc >= threshold_correlation) else x[np.argmax(fsc < threshold_correlation)]
+    log(f'Map-map FSC falls below correlation {threshold_correlation} at resolution {Apix/threshold_resolution} Å ({threshold_resolution} 1/px)')
+    utils.save_pkl((x, fsc), f'{args.o.split(".mrc")[0]}_FSC.pkl')
+
+    # plot FSC
+    plt.plot(x / Apix, fsc)
+    plt.xlabel('Spatial frequency (1/Å)')
+    plt.ylabel('Half-map FSC')
+    plt.tight_layout()
+    plt.savefig(f'{args.o.split(".mrc")[0]}_FSC.png')
+
+    # apply lowpass filter
+    lowpass_target = Apix/threshold_resolution if args.lowpass is None else args.lowpass
+    log(f'Lowpass filtering to {lowpass_target} Å')
+    def lowpass_filter(vol_ft, angpix, lowpass):
+        # get real space box width, and correct for FFT 1-px padding if applied
+        D = vol_ft.shape[0]
+        assert D % 2 == 1, f'Fourier transformed volume must have odd box length, found box length: {D}'
+
+        # calculate frequencies along one axis in units of 1/Å
+        lowest_freq = 1 / (D - 1)
+        freqs_1d_px = np.arange(start=-1 / 2, stop=1 / 2 + lowest_freq, step=lowest_freq)
+        freqs_1d_angstrom = freqs_1d_px / angpix
+
+        # create 3D mask binarized at lowpass frequency
+        freqs_x, freqs_y, freqs_z = np.meshgrid(freqs_1d_angstrom, freqs_1d_angstrom, freqs_1d_angstrom)
+        lowpass_mask = np.where(np.power(freqs_x ** 2 + freqs_y ** 2 + freqs_z ** 2, 0.5) <= 1 / lowpass,
+                                True,
+                                False)
+
+        # apply mask to volume to zero-out higher frequency components
+        if torch.is_tensor(vol_ft):
+            lowpass_mask = torch.tensor(lowpass_mask).to(vol_ft.device)
+        vol_ft = vol_ft * lowpass_mask
+
+        return vol_ft
+    V_lowpass = lowpass_filter(V, angpix = Apix, lowpass = lowpass_target)
+
+    # save volumes
+    def save_map(vol, vol_path, Apix):
+        vol = fft.ihtn_center(vol[0:-1,0:-1,0:-1].cpu().numpy())
+        if args.flip: vol = vol[::-1]
+        mrc.write(vol_path, vol.astype('float32'), Apix=Apix)
+        log(f'Wrote {vol_path}')
+    save_map(V, args.o, Apix)
+    save_map(V_lowpass, f'{args.o.split(".mrc")[0]}_filt.mrc', Apix)
+    save_map(V_half1, f'{args.o.split(".mrc")[0]}_half1.mrc', Apix)
+    save_map(V_half2, f'{args.o.split(".mrc")[0]}_half2.mrc', Apix)
+    log('Done!')
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__)
