c2e105391ac24fdb91aa48be254c8c787e1ad92c
bpowell122 <81090946+bpowell122@users.noreply.github.com>
2024-08-20 20:46:50 -0400

    add print_config function; add model type as config dict key; make config meta key immediately accessible

--- tomodrgn/config.py
+++ tomodrgn/config.py
@@ -2,7 +2,6 @@
 Functions to create, edit, and view tomoDRGN config.pkl files
 """
 import argparse
-import pickle
 import sys
 from datetime import datetime as dt
 from pprint import pprint
@@ -64,7 +63,8 @@ def save_config(args: argparse.Namespace,
                          sort_ptcl_imgs=star.sort_ptcl_imgs,
                          use_first_ntilts=star.use_first_ntilts,
                          use_first_nptcls=star.use_first_nptcls,
-                         sourcefile_filtered=star.sourcefile_filtered)
+                         sourcefile_filtered=star.sourcefile_filtered,
+                         type=type(star))
 
     dataset_args = dict(star_random_subset=data.star_random_subset,
                         datadir=data.datadir,
@@ -78,11 +78,13 @@ def save_config(args: argparse.Namespace,
                         recon_tilt_weight=data.recon_tilt_weight,
                         l_dose_mask=data.l_dose_mask,
                         constant_mintilt_sampling=data.constant_mintilt_sampling,
-                        sequential_tilt_sampling=data.sequential_tilt_sampling)
+                        sequential_tilt_sampling=data.sequential_tilt_sampling,
+                        type=type(data))
 
     lattice_args = dict(boxsize=lat.boxsize,
                         extent=lat.extent,
-                        ignore_DC=lat.ignore_dc)
+                        ignore_DC=lat.ignore_dc,
+                        type=type(lat))
 
     if type(model) is TiltSeriesHetOnlyVAE:
         model_args = dict(in_dim=model.encoder.in_dim,
@@ -103,7 +105,8 @@ def save_config(args: argparse.Namespace,
                           num_heads=args.num_heads,
                           layer_norm=args.layer_norm,
                           pe_type=args.pe_type,
-                          pe_dim=args.pe_dim)
+                          pe_dim=args.pe_dim,
+                          type=type(model))
         training_args = dict(n=args.num_epochs,
                              B=args.batch_size,
                              wd=args.wd,
@@ -126,7 +129,8 @@ def save_config(args: argparse.Namespace,
                           activation=args.activation,
                           pe_type=args.pe_type,
                           pe_dim=args.pe_dim,
-                          feat_sigma=args.feat_sigma,)
+                          feat_sigma=args.feat_sigma,
+                          type=type(model))
         training_args = dict(n=args.num_epochs,
                              B=args.batch_size,
                              wd=args.wd,
@@ -143,19 +147,19 @@ def save_config(args: argparse.Namespace,
     else:
         raise TypeError(f'Unsupported model type: {type(model)}')
 
+    meta = dict(time=dt.now(),
+                cmd=sys.argv,
+                version=tomodrgn.__version__)  # TODO replace with something like https://discuss.python.org/t/usual-pythonic-way-to-add-inject-the-git-hash-to-the-version-string/45169/10
+
     config = dict(starfile_args=starfile_args,
                   dataset_args=dataset_args,
                   lattice_args=lattice_args,
                   model_args=model_args,
-                  training_args=training_args)
+                  training_args=training_args,
+                  meta=meta)
     config['seed'] = args.seed
     config['angpix'] = star.get_tiltseries_pixelsize()
-    with open(out_config, 'wb') as f:
-        pickle.dump(config, f)
-        meta = dict(time=dt.now(),
-                    cmd=sys.argv,
-                    version=tomodrgn.__version__)
-        pickle.dump(meta, f)
+    utils.save_pkl(data=config, out_pkl=out_config)
 
 
 def print_config(config: str | dict) -> None:
@@ -164,5 +168,10 @@ def print_config(config: str | dict) -> None:
     :param config: path to config.pkl file, or preloaded config dict
     :return: None
     """
-    # TODO pprint every sub-dict
-    raise NotImplementedError
+    # check inputs
+    if type(config) is str:
+        config = utils.load_pkl(config)
+    assert type(config) is dict, f'Unrecognized config type: {type(config)}'
+
+    # display the config contents
+    pprint(config)
