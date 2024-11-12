27be2fd9eae1d54b405b812493f16e54a66f9860
bpowell122 <bmp@mit.edu>
2024-11-12 16:52:52 -0500

    add WarpTools star file and float16 image compatibility

--- tomodrgn/commands/downsample.py
+++ tomodrgn/commands/downsample.py
@@ -4,6 +4,7 @@ Downsample an image stack or volume by Fourier cropping
 import argparse
 import math
 import os
+from typing import get_args
 
 import numpy as np
 import torch
@@ -25,6 +26,8 @@ def add_args(parser: argparse.ArgumentParser | None = None) -> argparse.Argument
     parser.add_argument('input', help='Input particles or volume (.mrc, .mrcs, .star, or .txt)')
 
     group = parser.add_argument_group('Core arguments')
+    group.add_argument('--source-software', type=str, choices=get_args(starfile.KNOWN_STAR_SOURCES), default='auto',
+                       help='Manually set the software used to extract particles. Default is to auto-detect.')
     group.add_argument('--downsample', type=int, required=True, help='New box size in pixels, must be even')
     group.add_argument('--output', metavar='MRCS', type=os.path.abspath, required=True, help='Output projection stack (.mrcs)')
     group.add_argument('--batch-size', type=int, default=5000, help='Batch size for processing images')
@@ -90,20 +93,23 @@ def write_downsampled_starfile(input_starfile: str,
                                boxsize_old: int,
                                boxsize_new: int,
                                out_mrcs: str | list[str],
-                               chunk_size: int | None = None) -> None:
+                               chunk_size: int | None = None,
+                               source_software: starfile.KNOWN_STAR_SOURCES = 'auto') -> None:
     """
-    Write a TiltSeriesStarfile describing a downsampled particle stack (optionally chunked into multiple .mrcs files) to disk.
+    Write a STAR file describing a downsampled particle stack (optionally chunked into multiple .mrcs files) to disk.
 
     :param input_starfile: path to the star file used to reference original (non-downsampled) images
     :param boxsize_old: the original box size (units: px)
     :param boxsize_new: the new box size after Fourier cropping (units: px)
     :param out_mrcs: path to the downsampled mrcs file(s), referencing images in the same order as the original star file
     :param chunk_size: number of images chunked together in separate .mrcs files written to disk, if used. None means no chunking was performed.
+        :param source_software: type of source software used to create the star file, used to indicate the appropriate star file handling class to instantiate.
+            Default of 'auto' tries to infer the appropriate star file handling class based on whether ``star_path`` is an optimisation set star file.
     :return: None
     """
     # load the star file
     assert input_starfile.endswith('.star')
-    star = starfile.TiltSeriesStarfile(input_starfile)
+    star = starfile.load_sta_starfile(input_starfile, source_software=source_software)
 
     # get new pixel size according to star file
     old_angpix = star.get_tiltseries_pixelsize()
@@ -129,6 +135,11 @@ def write_downsampled_starfile(input_starfile: str,
         out_star = f'{os.path.splitext(os.path.splitext(out_mrcs[0])[0])[0]}.star'  # convert FOO.0.mrcs to FOO
     star.df[star.header_ptcl_image] = new_paths[:len(star.df)]
 
+    # if input is optimisation set star file, ensure output is optimisation set star file
+    if type(star) is starfile.TomoParticlesStarfile:
+        if '_optimisation_set' not in out_star:
+            out_star = f'{os.path.splitext(out_star)[0]}_optimisation_set.star'
+
     # save the star file
     star.write(out_star)
 
@@ -139,20 +150,61 @@ class ImageDataset(torch.utils.data.Dataset):
     """
 
     def __init__(self,
-                 particles: np.ndarray | list[mrc.LazyImage],
-                 nimgs: int):
+                 particles: np.ndarray | list[mrc.LazyImage] | list[mrc.LazyImageStack]):
         self.particles = particles
-        self.nimgs = nimgs
+        if type(particles[0]) is np.ndarray:
+            self.ptcl_img_indices = np.asarray([i for i in range(len(particles))])
+            self.nimgs = len(particles)
+        elif type(particles[0]) is mrc.LazyImage:
+            self.ptcl_img_indices = np.asarray([i for i in range(len(particles))])
+            self.nimgs = len(particles)
+        elif type(particles[0]) is mrc.LazyImageStack:
+            ptcl_img_indices = []
+            offset = 0
+            for ptcl in particles:
+                ptcl_img_indices.append(np.arange(offset, offset + ptcl.n_images))
+                offset += ptcl.n_images
+            self.ptcl_img_indices = ptcl_img_indices
+            self.nimgs = self.ptcl_img_indices[-1][-1] + 1  # correct for 0-indexing
+        else:
+            raise ValueError(f'Unsupported particles type: {type(particles[0])}')
 
     def __len__(self) -> int:
-        return self.nimgs
+        return len(self.ptcl_img_indices)
 
     def __getitem__(self,
-                    index: int) -> tuple[int, np.ndarray]:
-        if type(self.particles[index]) is mrc.LazyImage:
-            return index, self.particles[index].get()
-        else:
-            return index, self.particles[index]
+                    index: int) -> tuple[np.ndarray, np.ndarray]:
+        particles = self.particles[index] if type(self.particles[index]) is np.ndarray else self.particles[index].get()
+        # cast to float32 because input data may be float16 which is not supported by cuFFT (https://github.com/pytorch/pytorch/issues/70664)
+        particles = particles.astype(np.float32)
+        # get the indices of where this/these images will go in the array of all images
+        indices = np.asarray(self.ptcl_img_indices[index])
+        return indices, particles
+
+
+def collate_particle_tilts(batch: list[tuple[np.ndarray, np.ndarray]]) -> tuple[torch.tensor, torch.tensor]:
+    """
+    Collate a batch of particle images, optionally with a variable number of tilt images associated with each particle.
+    Given input of shape ``([ind1], [a, X, X]), ([ind2], [b, X, X])`` where a and b can be any number, returns ``[ind1, ind2], [[a+b, X, X]]``.
+
+    :param batch: list of 2-tuples of particle indices (``np.ndarray`` of shape ``(ntilts)``) and associated images (``np.ndarray`` of shape ``(ntilts, npixels, npixels)``
+    :returns: 2-tuple of concatenated particle indices and concatenated particle images
+    """
+    collated_indices = []
+    collated_images = []
+    for i in range(len(batch)):
+        collated_indices.append(torch.as_tensor(batch[i][0]))
+        collated_images.append(torch.as_tensor(batch[i][1]))
+
+    collated_indices = torch.hstack(collated_indices)
+    if collated_images[0].ndim == 2:
+        collated_images = torch.stack(collated_images)
+    elif collated_images[0].ndim == 3:
+        collated_images = torch.concatenate(collated_images, dim=0)
+    else:
+        raise ValueError(f'Unrecognized leading shape of image data: {collated_images[0].shape}')
+
+    return collated_indices, collated_images
 
 
 def main(args):
@@ -172,8 +224,9 @@ def main(args):
     lazy = args.lazy
     old = dataset.load_particles(mrcs_txt_star=args.input,
                                  lazy=lazy,
-                                 datadir=args.datadir)
-    nimgs = len(old)
+                                 datadir=args.datadir,
+                                 source_software=args.source_software)
+    old_dtype = old.dtype if type(old) is np.ndarray else old[0].get().dtype
     boxsize_old = old[0].get().shape[0] if lazy else old.shape[-1]
     boxsize_new = args.downsample
     assert boxsize_new <= boxsize_old, f'New box size {boxsize_new} cannot be larger than the original box size {boxsize_old}'
@@ -185,7 +238,7 @@ def main(args):
     if args.input.endswith('.mrcs') or args.input.endswith('.mrc'):
         old_angpix = mrc.parse_header(args.input).get_apix()
     elif args.input.endswith('.star'):
-        old_angpix = starfile.TiltSeriesStarfile(args.input).get_tiltseries_pixelsize()
+        old_angpix = starfile.load_sta_starfile(args.input, source_software=args.source_software).get_tiltseries_pixelsize()
     elif args.input.endswith('.txt'):
         with open(args.input, 'r') as f:
             old_angpix = mrc.parse_header(f.readline()).get_apix()
@@ -216,18 +269,18 @@ def main(args):
 
     # Downsample images in batches, saving to one .mrcs file
     elif args.chunk is None:
+        # iterate through image batches to apply downsampling and store in new volume array
+        particle_dataset = ImageDataset(particles=old)
+        data_generator = torch.utils.data.DataLoader(dataset=particle_dataset, batch_size=args.batch_size, shuffle=False, collate_fn=collate_particle_tilts)
+
         # prepare outputs
         out_mrcs = args.output
-        new = np.empty((nimgs, boxsize_new, boxsize_new), dtype=np.float32)
-
-        # iterate through image batches to apply downsampling and store in new volume array
-        particle_dataset = ImageDataset(particles=old, nimgs=nimgs)
-        data_generator = torch.utils.data.DataLoader(dataset=particle_dataset, batch_size=args.batch_size, shuffle=False)
+        new = np.empty((particle_dataset.nimgs, boxsize_new, boxsize_new), dtype=old_dtype)
         for batch_idx, batch_ptcls in data_generator:
             log(f'Processing indices {batch_idx[0]} - {batch_idx[-1]}')
             batch_ptcls.to(device)
             batch_new = downsample_images(batch_original=batch_ptcls, start=start, stop=stop)
-            new[batch_idx.cpu().numpy()] = batch_new.cpu().numpy()
+            new[batch_idx.cpu().numpy()] = batch_new.cpu().numpy().astype(old_dtype)
 
         # write to disk
         log(new.shape)
@@ -242,7 +295,8 @@ def main(args):
                                        boxsize_old=boxsize_old,
                                        boxsize_new=boxsize_new,
                                        out_mrcs=out_mrcs,
-                                       chunk_size=args.chunk)
+                                       chunk_size=args.chunk,
+                                       source_software=args.source_software)
 
     # Downsample images in batches, saving chunks of N images to nchunks separate .mrcs files
     else:
@@ -253,14 +307,14 @@ def main(args):
         for i in range(nchunks):
             log(f'Processing chunk {i}')
             chunk = old[i * args.chunk: (i + 1) * args.chunk]
-            new = np.empty((len(chunk), boxsize_new, boxsize_new), dtype=np.float32)
 
-            particle_dataset = ImageDataset(particles=chunk, nimgs=len(chunk))
-            data_generator = torch.utils.data.DataLoader(dataset=particle_dataset, batch_size=args.batch_size, shuffle=False)
+            particle_dataset = ImageDataset(particles=chunk)
+            data_generator = torch.utils.data.DataLoader(dataset=particle_dataset, batch_size=args.batch_size, shuffle=False, collate_fn=collate_particle_tilts)
+            new = np.empty((particle_dataset.nimgs, boxsize_new, boxsize_new), dtype=old_dtype)
             for batch_idx, batch_ptcls in data_generator:
                 log(f'Processing chunk indices {batch_idx[0]} - {batch_idx[-1]}')
                 batch_new = downsample_images(batch_original=batch_ptcls, start=start, stop=stop)
-                new[batch_idx.cpu().numpy()] = batch_new.cpu().numpy()
+                new[batch_idx.cpu().numpy()] = batch_new.cpu().numpy().astype(old_dtype)
 
             log(new.shape)
             log(f'Saving {out_mrcs[i]}')
@@ -280,7 +334,8 @@ def main(args):
                                        boxsize_old=boxsize_old,
                                        boxsize_new=boxsize_new,
                                        out_mrcs=out_mrcs,
-                                       chunk_size=args.chunk)
+                                       chunk_size=args.chunk,
+                                       source_software=args.source_software)
 
 
 if __name__ == '__main__':
