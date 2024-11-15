bugfix numerical overflows when calculating dataset normalization

tomodrgn/dataset.py
@@ -332,11 +332,13 @@ class TiltSeriesMRCData(data.Dataset):
                 random_imgs_for_normalization = np.random.choice(np.arange(nimgs),
                                                                  size=nimgs // 100,
                                                                  replace=False)
-                norm = [np.mean(particles[random_imgs_for_normalization]),
-                        np.std(particles[random_imgs_for_normalization])]
+                random_imgs_for_normalization = particles[random_imgs_for_normalization].astype(np.float32)
+                norm = [np.mean(random_imgs_for_normalization),
+                        np.std(random_imgs_for_normalization)]
                 norm[0] = 0
             else:
                 norm = self.norm
+            assert np.isfinite(norm[0]) and np.isfinite(norm[1]), f'Normalization mean and/or standard deviation are not finite: {norm}'
             particles -= norm[0]  # zero mean
             particles /= norm[1]  # unit stdev, separate line required to avoid redundant memory allocation
             utils.log(f'Normalized HT by mean offset {norm[0]} and standard deviation scaling {norm[1]}')
@@ -357,6 +359,7 @@ class TiltSeriesMRCData(data.Dataset):
         n = min(10000, self.nimgs)
         random_imgs_for_normalization = np.random.choice(self.nimgs, size=n, replace=False)
         imgs = np.asarray([self.ptcls[i].get() for i in random_imgs_for_normalization])
+        imgs = imgs.astype(np.float32)  # higher precision to avoid internal numpy overflow when calculating mean
         if self.window:
             imgs *= self.real_space_2d_mask
         for (i, img) in enumerate(imgs):
@@ -366,6 +369,7 @@ class TiltSeriesMRCData(data.Dataset):
         imgs = fft.symmetrize_ht(imgs)
         norm = [np.mean(imgs), np.std(imgs)]
         norm[0] = 0.0
+        assert np.isfinite(norm[0]) and np.isfinite(norm[1]), f'Normalization mean and/or standard deviation are not finite: {norm}'
         utils.log(f'Normalizing HT by {norm[0]} +/- {norm[1]}')
         return norm
 
@@ -868,11 +872,13 @@ class TomoParticlesMRCData(data.Dataset):
                 random_imgs_for_normalization = np.random.choice(np.arange(self.nimgs),
                                                                  size=self.nimgs // 100,
                                                                  replace=False)
-                norm = [np.mean(particles[random_imgs_for_normalization]),
-                        np.std(particles[random_imgs_for_normalization])]
+                random_imgs_for_normalization = particles[random_imgs_for_normalization].astype(np.float32)
+                norm = [np.mean(random_imgs_for_normalization),
+                        np.std(random_imgs_for_normalization)]
                 norm[0] = 0
             else:
                 norm = self.norm
+            assert np.isfinite(norm[0]) and np.isfinite(norm[1]), f'Normalization mean and/or standard deviation are not finite: {norm}'
             particles -= norm[0]  # zero mean
             particles /= norm[1]  # unit stdev, separate line required to avoid redundant memory allocation
             utils.log(f'Normalized HT by mean offset {norm[0]} and standard deviation scaling {norm[1]}')
@@ -987,6 +993,7 @@ class TomoParticlesMRCData(data.Dataset):
         n = min(500, self.nptcls)
         random_ptcls_for_normalization = np.random.choice(self.nptcls, size=n, replace=False)
         imgs = np.concatenate([self.ptcls[i].get(low_memory=False) for i in random_ptcls_for_normalization])
+        imgs = imgs.astype(np.float32)  # higher precision to avoid internal numpy overflow when calculating mean
         if self.window:
             imgs *= self.real_space_2d_mask
         for (i, img) in enumerate(imgs):
@@ -994,9 +1001,9 @@ class TomoParticlesMRCData(data.Dataset):
         if self.invert_data:
             imgs *= -1
         imgs = fft.symmetrize_ht(imgs)
-        imgs = imgs.astype(np.float64)  # higher precision to avoid internal numpy overflow when calculating mean
         norm = [np.mean(imgs), np.std(imgs)]
         norm[0] = 0.0
+        assert np.isfinite(norm[0]) and np.isfinite(norm[1]), f'Normalization mean and/or standard deviation are not finite: {norm}'
         utils.log(f'Normalizing HT by {norm[0]} +/- {norm[1]}')
         return norm
 